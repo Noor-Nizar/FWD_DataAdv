2f4ba2c5dacd1c1bf5d36aadefc30d7c8faef22d
Noor-Nizar <88066634+Noor-Nizar@users.noreply.github.com>
2022-02-23 21:06:45 +0200

    q8_update

--- q8.sql
+++ q8.sql
@@ -1,3 +1,4 @@
+-- use this to get least amount of sales for 10th top artist.
 WITH T AS
   (SELECT a.ArtistId,
           sum(il.quantity) amtsold
@@ -7,8 +8,8 @@ WITH T AS
    JOIN InvoiceLine il ON il.TrackId = t.TrackId
    GROUP BY 1
    ORDER BY 2 DESC
-   LIMIT 10),
-     TA AS
+   LIMIT 10), --incase there are some artists sharing the same sales as 10th
+TA AS
   (SELECT a.artistid,
           a.Name
    FROM Artist a
@@ -19,8 +20,8 @@ WITH T AS
    HAVING SUM(il.quantity) >=
      (SELECT MIN(T.amtsold)
       FROM T)
-   ORDER BY 1),
-     TGA AS
+   ORDER BY 1), --get the total sales per genre for each artist
+ TGA AS
   (SELECT ta.ArtistId,
           g.GenreId,
           SUM(il.Quantity) cg
@@ -30,21 +31,50 @@ WITH T AS
    JOIN Genre g ON T.GenreId = g.GenreId
    JOIN InvoiceLine il ON T.TrackId = il.TrackId
    GROUP BY 1,
-            2),
-     TGM AS
+            2), --get the max genre of sales for each artist
+ TGM AS
   (SELECT tga.ArtistId,
           tga.GenreId,
           MAX(tga.cg) cg
    FROM TGA
    JOIN T ON tga.ArtistId = T.ArtistId
-   GROUP BY 1)
+   GROUP BY 1), --counts the number of artists' other than top genre's tracks released
+ tcnpg AS
+  (SELECT stgm.ArtistId,
+          count(*) cnpg
+   FROM TGM stgm
+   JOIN album al ON al.ArtistId = stgm.ArtistId
+   JOIN Track t ON t.AlbumId = al.AlbumId
+   WHERE t.GenreId !=
+       (SELECT tgm.GenreId
+        FROM tgm
+        WHERE stgm.ArtistId = tgm.ArtistId)
+   GROUP BY 1), --counts the number of artists' other than top genre's tracks released
+ tcpg AS
+  (SELECT stgm.ArtistId,
+          count(*) cpg
+   FROM TGM stgm
+   JOIN album al ON al.ArtistId = stgm.ArtistId
+   JOIN Track t ON t.AlbumId = al.AlbumId
+   WHERE t.GenreId =
+       (SELECT tgm.GenreId
+        FROM tgm
+        WHERE stgm.ArtistId = tgm.ArtistId)
+   GROUP BY 1) --joins the data
+
 SELECT a.Name,
        t.amtsold,
        g.Name Genre,
-       tgm.cg
+       tgm.cg,
+       coalesce(tcnpg.cnpg, 0),
+       tcpg.cpg
 FROM TGM
+LEFT JOIN tcnpg ON tgm.ArtistId = tcnpg.ArtistId
 JOIN T ON tgm.ArtistId = t.ArtistId
+JOIN tcpg ON tgm.ArtistId = tcpg.ArtistId
 JOIN Genre g ON g.GenreId = tgm.GenreId
 JOIN Artist a ON a.ArtistId = tgm.ArtistId
 ORDER BY 2 DESC,
-         4 DESC
+         4 DESC,
+         5 DESC,
+         6 DESC
